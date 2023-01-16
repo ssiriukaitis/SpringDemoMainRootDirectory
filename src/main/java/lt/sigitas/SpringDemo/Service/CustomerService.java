97975f05715af7791829d91806e723068191ab30
ssiriukaitis <sigitas.siriukaitis@gmail.com>
2023-01-16 15:42:27 +0200

    Add customerService klase

---
+++ src/main/java/lt/sigitas/SpringDemo/Service/CustomerService.java
@@ -0,0 +1,20 @@
+package lt.sigitas.SpringDemo.Service;
+
+import lt.sigitas.SpringDemo.Repository.Customer;
+import lt.sigitas.SpringDemo.Repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+
+    public List<Customer> getAllCustomers(){
+        return (List<Customer>) customerRepository.findAll();
+    }
+}
