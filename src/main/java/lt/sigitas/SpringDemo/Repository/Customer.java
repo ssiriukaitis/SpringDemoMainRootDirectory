61bdaa70324bac1ce230e3ebd8a8c51e66ec05ee
ssiriukaitis <sigitas.siriukaitis@gmail.com>
2023-01-16 14:44:25 +0200

    Automatiniai geteriai, seteriai ir toStringas veikia

--- src/main/java/lt/sigitas/SpringDemo/Repository/Customer.java
+++ src/main/java/lt/sigitas/SpringDemo/Repository/Customer.java
@@ -5,9 +5,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 
 @Entity
 @Table(name = "customers")
+@Getter @Setter @NoArgsConstructor @ToString
 public class Customer {
 
     @Id
@@ -39,126 +45,25 @@ public class Customer {
     @Column(name = "creditlimit")
     private Double creditLimit;
 
-    public Integer getCustomerNumber() {
-        return customerNumber;
-    }
 
-    public void setCustomerNumber(Integer customerNumber) {
-        this.customerNumber = customerNumber;
-    }
 
-    public String getCustomerName() {
-        return customerName;
-    }
 
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-    public String getContactLastName() {
-        return contactLastName;
-    }
-
-    public void setContactLastName(String contactLastName) {
-        this.contactLastName = contactLastName;
-    }
-
-    public String getContactFirstName() {
-        return contactFirstName;
-    }
-
-    public void setContactFirstName(String contactFirstName) {
-        this.contactFirstName = contactFirstName;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getAddressLine1() {
-        return addressLine1;
-    }
-
-    public void setAddressLine1(String addressLine1) {
-        this.addressLine1 = addressLine1;
-    }
-
-    public String getAddressLine2() {
-        return addressLine2;
-    }
-
-    public void setAddressLine2(String addressLine2) {
-        this.addressLine2 = addressLine2;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public Integer getSalesRepEmployeeNumber() {
-        return salesRepEmployeeNumber;
-    }
-
-    public void setSalesRepEmployeeNumber(Integer salesRepEmployeeNumber) {
-        this.salesRepEmployeeNumber = salesRepEmployeeNumber;
-    }
-
-    public Double getCreditLimit() {
-        return creditLimit;
-    }
-
-    public void setCreditLimit(Double creditLimit) {
-        this.creditLimit = creditLimit;
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "customerNumber=" + customerNumber +
-                ", customerName='" + customerName + '\'' +
-                ", contactLastName='" + contactLastName + '\'' +
-                ", contactFirstName='" + contactFirstName + '\'' +
-                ", phone='" + phone + '\'' +
-                ", addressLine1='" + addressLine1 + '\'' +
-                ", addressLine2='" + addressLine2 + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", postalCode='" + postalCode + '\'' +
-                ", country='" + country + '\'' +
-                ", salesRepEmployeeNumber=" + salesRepEmployeeNumber +
-                ", creditLimit=" + creditLimit +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "Customer{" +
+//                "customerNumber=" + customerNumber +
+//                ", customerName='" + customerName + '\'' +
+//                ", contactLastName='" + contactLastName + '\'' +
+//                ", contactFirstName='" + contactFirstName + '\'' +
+//                ", phone='" + phone + '\'' +
+//                ", addressLine1='" + addressLine1 + '\'' +
+//                ", addressLine2='" + addressLine2 + '\'' +
+//                ", city='" + city + '\'' +
+//                ", state='" + state + '\'' +
+//                ", postalCode='" + postalCode + '\'' +
+//                ", country='" + country + '\'' +
+//                ", salesRepEmployeeNumber=" + salesRepEmployeeNumber +
+//                ", creditLimit=" + creditLimit +
+//                '}';
+//    }
 }
--- src/main/java/lt/sigitas/SpringDemo/Repository/ProducLine.java
+++ src/main/java/lt/sigitas/SpringDemo/Repository/ProducLine.java
@@ -5,9 +5,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "productlines")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class ProducLine {
 
     @Id
@@ -19,38 +27,7 @@ public class ProducLine {
     @Column(name = "htmldescription")
     private String htmlDescription;
 
-    public String getProductLine() {
-        return productLine;
-    }
 
-    public void setProductLine(String productLine) {
-        this.productLine = productLine;
-    }
-
-    public String getTextDescription() {
-        return textDescription;
-    }
-
-    public void setTextDescription(String textDescription) {
-        this.textDescription = textDescription;
-    }
-
-    public String getHtmlDescription() {
-        return htmlDescription;
-    }
-
-    public void setHtmlDescription(String htmlDescription) {
-        this.htmlDescription = htmlDescription;
-    }
-
-    @Override
-    public String toString() {
-        return "ProductLine{" +
-                "productLine=" + productLine +
-                ", textDescription='" + textDescription + '\'' +
-                ", htmlDescription='" + htmlDescription +
-                '}';
-    }
 }
 
 
